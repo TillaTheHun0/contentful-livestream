d2e508c53f1ee6395d90df007a64b0f94bf324ef
Tyler Hall <0tillathehun0@gmail.com>
2020-11-29 21:08:57 -0500

    feat(pokemon): allow multiple contentful envs based on branch name

--- packages/pokemon/app/functions/graphql/schemas/contentful/config.js
+++ packages/pokemon/app/functions/graphql/schemas/contentful/config.js
@@ -2,4 +2,7 @@
 export const CONTENTFUL_MANAGEMENT_API_KEY = process.env.CONTENTFUL_MANAGEMENT_API_KEY
 export const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID
 
-export const CONTENTFUL_SPACE_ENVIRONMENT = process.env.CONTENTFUL_SPACE_ENVIRONMENT || 'master'
+/**
+ * * Gitflow branch name mapping ie. feat/foo/feature -> feat-foo-feature
+ */
+export const CONTENTFUL_SPACE_ENVIRONMENT = process.env.CONTENTFUL_SPACE_ENVIRONMENT || `GH-${process.env.VERCEL_GIT_COMMIT_REF.replace(/\//g, '-')}`
