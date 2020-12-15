1b6e9acf22f8d7d4b130f97ccedbee6646cf23aa
Tyler Hall <0tillathehun0@gmail.com>
2020-12-15 12:13:20 -0500

    feat(pokemon): add relatedPokeTypes field migration

---
+++ packages/pokemon/contentful-migrations/src/2.js
@@ -0,0 +1,64 @@
+
+const { ApolloClient, HttpLink, InMemoryCache, gql } = require('@apollo/client')
+const fetch = require('cross-fetch')
+
+const POKEMON_BLOG_TYPE = 'pokemonBlog'
+const RELATED_TYPES_FIELD = 'relatedPokeTypes'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://pokemon-lover.vercel.app/api/graphql', fetch }),
+  cache: new InMemoryCache()
+})
+
+const POKEMON_TYPES_QUERY = gql`
+  query GetPokemonTypes ($name: String!) {
+    pokemon (name: $name) {
+      name
+      types
+    }
+  }
+`
+
+// @ts-check
+/** @type {import('contentful-migration').MigrationFunction} */
+module.exports = async function (migration) {
+  const pokemonBlogType = migration.editContentType(POKEMON_BLOG_TYPE)
+
+  pokemonBlogType.createField(RELATED_TYPES_FIELD)
+    .name('Related Poke Types')
+    .type('Array')
+    .items({
+      type: 'Symbol',
+      validations: [
+        {
+          in: [
+            'bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 'fire', 'flying', 'ghost',
+            'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water'
+          ]
+        }
+      ]
+    })
+
+  try {
+    migration.transformEntries({
+      contentType: POKEMON_BLOG_TYPE,
+      from: ['name'],
+      to: [RELATED_TYPES_FIELD],
+      transformEntryForLocale: async (from, locale) => {
+        const { data } = await client.query({
+          query: POKEMON_TYPES_QUERY,
+          variables: { name: from.name[locale] }
+        })
+
+        const types = data.pokemon.types
+
+        return {
+          [RELATED_TYPES_FIELD]: types
+        }
+      }
+    })
+  } catch (err) {
+    console.error(err)
+    pokemonBlogType.deleteField()
+  }
+}
